5a9dd7cb6e9a7cfcb832b59aec8a53b07bac60ab
jyaunches <jmyaunch@gmail.com>
2016-10-26 14:06:46 -0400

    initial project config

---
+++ trump-alarmTests/trump_alarmTests.swift
@@ -0,0 +1,36 @@
+//
+//  trump_alarmTests.swift
+//  trump-alarmTests
+//
+//  Created by Julietta Yaunches on 10/26/16.
+//  Copyright © 2016 yaunches. All rights reserved.
+//
+
+import XCTest
+@testable import trump_alarm
+
+class trump_alarmTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
