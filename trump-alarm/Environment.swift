c3c38453aa8ca9168f37af7dff287e82380b60c6
Stephanie Guevara <stephanie.n.guevara@gmail.com>
2016-10-29 14:16:57 -0400

    added color constants

---
+++ trump-alarm/Environment.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  trump-alarm
+//
+//  Created by Stephanie Guevara on 10/29/16.
+//  Copyright © 2016 yaunches. All rights reserved.
+//
+
+import UIKit
+
+struct Environment {
+    
+    struct Styling {
+        static let trumpGold = UIColor(red: 228.0/255.0, green: 201.0/255.0, blue: 55.0/255.0, alpha: 1.0)
+        static let trumpRed = UIColor(red: 165.0/255.0, green: 30.0/255.0, blue: 35.0/255.0, alpha: 1.0)
+    }
+}
