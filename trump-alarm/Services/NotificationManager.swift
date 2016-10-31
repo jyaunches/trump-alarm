fb74933a3131dc46da725387e3cd727c7d7b5ecc
jyaunches <jmyaunch@gmail.com>
2016-10-30 21:31:22 -0400

    Updating notifications to contain content and audiofile names.

--- trump-alarm/Services/NotificationManager.swift
+++ trump-alarm/Services/NotificationManager.swift
@@ -24,19 +24,19 @@ class NotificationManager: NSObject, UNUserNotificationCenterDelegate {
 
     func setupPrePolling(pollingDate: Date) {
         let prePollsQuotes = [
-            TrumpQuote(content: "Such a nasty woman.", audioFile: "nasty_woman.wav", identifier: "nasty-woman"),
-                TrumpQuote(content: "Bomb the shit out of em.", audioFile: "bomb-the-shit.wav", identifier: "bomb-the-shit"),
-                TrumpQuote(content: "Creeping miss teen.", audioFile: "creeping-miss-teen.wav", identifier: "creeping-miss-teen"),
-                TrumpQuote(content: "Such a nasty woman.", audioFile: "nasty_woman.wav", identifier: "foo123"),
-                TrumpQuote(content: "Bomb the shit out of em.", audioFile: "bomb-the-shit.wav", identifier: "foo234"),
-                TrumpQuote(content: "Creeping miss teen.", audioFile: "creeping-miss-teen.wav", identifier: "foo345"),
-                TrumpQuote(content: "Such a nasty woman.", audioFile: "bomb-the-shit.wav", identifier: "foo678"),
-                TrumpQuote(content: "Bomb the shit out of em.", audioFile: "bomb-the-shit.wav", identifier: "foo567"),
+                TrumpQuote(content: SoundBiteContent.grabEm, audioFile: "grab-em.wav", identifier: "nasty-woman"),
+                TrumpQuote(content: SoundBiteContent.datingIvanka, audioFile: "dating-Ivanka.wav", identifier: "bomb-the-shit"),
+                TrumpQuote(content: SoundBiteContent.droppingToKnees, audioFile: "dropping-to-your-knees.wav", identifier: "creeping-miss-teen"),
+                TrumpQuote(content: SoundBiteContent.muslimShutdown, audioFile: "muslim-shutdown.wav", identifier: "foo123"),
+                TrumpQuote(content: SoundBiteContent.meganKelly, audioFile: "megan-kelly-blood.wav", identifier: "foo234"),
+                TrumpQuote(content: SoundBiteContent.nastyWoman, audioFile: "nasty_woman.wav", identifier: "foo345"),
+                TrumpQuote(content: SoundBiteContent.fifthAve, audioFile: "I-could-shoot-somebody.wav", identifier: "foo678"),
+                TrumpQuote(content: SoundBiteContent.bombTheShit, audioFile: "bomb-the-shit.wav", identifier: "foo567"),
         ]
 
         requestNotificationPermission(onAgree: {
             let intervals: [TimeInterval] = Date.intervalsUntilElectionDay()
-            
+
             for (index, interval) in intervals.enumerated() {
                 let actualAlarmDate = Date(timeIntervalSinceReferenceDate: Date().timeIntervalSinceReferenceDate + interval)
                 print("Scheduling alarm for: \(actualAlarmDate.standardPrint()))")
@@ -48,13 +48,25 @@ class NotificationManager: NSObject, UNUserNotificationCenterDelegate {
 
     func setupPollingDay() {
         let pollingDayQuotes = [
-                TrumpQuote(content: "Creeping miss teen.", audioFile: "creeping-miss-teen.wav", identifier: "foo234"),
-                TrumpQuote(content: "Such a nasty woman.", audioFile: "nasty_woman.wav", identifier: "foo234"),
-                TrumpQuote(content: "Bomb the shit out of em.", audioFile: "bomb-the-shit.wav", identifier: "foo234"),
-                TrumpQuote(content: "Creeping miss teen.", audioFile: "creeping-miss-teen.wav", identifier: "foo234"),
-                TrumpQuote(content: "Creeping miss teen.", audioFile: "creeping-miss-teen.wav", identifier: "foo234"),
-                TrumpQuote(content: "Such a nasty woman.", audioFile: "nasty_woman.wav", identifier: "foo234"),
-                TrumpQuote(content: "Bomb the shit out of em.", audioFile: "bomb-the-shit.wav", identifier: "foo234")
+                TrumpQuote(content: SoundBiteContent.waterboarding, audioFile: "waterboarding.wav", identifier: "foo234"),
+                TrumpQuote(content: SoundBiteContent.handSize, audioFile: "hand-size.wav", identifier: "foo234"),
+                TrumpQuote(content: SoundBiteContent.obamaCofoundedIsis, audioFile: "obama-clinton-co-founded-ISIS.wav", identifier: "foo234"),
+                TrumpQuote(content: SoundBiteContent.fuckHerSheWasMarried, audioFile: "try-and-fuck-her-she-was-married.wav", identifier: "foo234"),
+                TrumpQuote(content: SoundBiteContent.mccainNotAHero, audioFile: "not-a-war-hero.wav", identifier: "foo234"),
+                TrumpQuote(content: SoundBiteContent.secondAmendment, audioFile: "second-ammendment.wav", identifier: "foo234"),
+                TrumpQuote(content: SoundBiteContent.takeTheirGuns, audioFile: "take-their-guns-away.wav", identifier: "foo234"),
+                TrumpQuote(content: SoundBiteContent.buildAWall, audioFile: "build-a-wall.wav", identifier: "foo234"),
+                TrumpQuote(content: SoundBiteContent.noPuppet, audioFile: "you're-the-puppet.wav", identifier: "foo234"),
+                TrumpQuote(content: SoundBiteContent.nucs, audioFile: "won't-rule-out-nuclear-weapons.wav", identifier: "foo234"),
+                TrumpQuote(content: SoundBiteContent.relationshipWithTheBlacks, audioFile: "great-relationship-with-the-blacks.wav", identifier: "foo234"),
+                TrumpQuote(content: SoundBiteContent.teenCreeping, audioFile: "creeping-miss-teen.wav", identifier: "foo234"),
+                TrumpQuote(content: SoundBiteContent.theyreRapists, audioFile: "they're-rapist.wav", identifier: "foo234"),
+                TrumpQuote(content: SoundBiteContent.carryOutInAStretcher, audioFile: "carried-out-in-a-stretcher.wav", identifier: "foo234"),
+                /*
+                TrumpQuote(content: SoundBiteContent.obamaPositiveImactOnThugs, audioFile: "bomb-the-shit.wav", identifier: "foo234"),
+                TrumpQuote(content: SoundBiteContent.iWontLoseAPenny, audioFile: "bomb-the-shit.wav", identifier: "foo234")
+                */
+
         ]
 
         requestNotificationPermission(onAgree: {
@@ -79,7 +91,7 @@ class NotificationManager: NSObject, UNUserNotificationCenterDelegate {
         content.title = "Wake up and Vote"
         content.body = quote.content
         content.sound = UNNotificationSound(named: quote.audioFile)
-        
+
         let trigger = UNTimeIntervalNotificationTrigger.init(timeInterval: interval, repeats: false)
         let request = UNNotificationRequest(identifier: quote.identifier, content: content, trigger: trigger)
 
---
+++ trump-alarm/SoundBites/SoundBiteContent.swift
@@ -0,0 +1,60 @@
+//
+//  SoundBiteContent.swift
+//  trump-alarm
+//
+//  Created by Julietta Yaunches on 10/30/16.
+//  Copyright © 2016 yaunches. All rights reserved.
+//
+
+import Foundation
+
+class SoundBiteContent {
+    static var grabEm = "I gotta use some Tic Tac just in case I start kissing her. You know I’m automatically attracted to beautiful — I just start kissing them. It’s like a magnet. Just kiss. I don’t even wait. And when you’re a star, they let you do it, you can do anything. Grab them by the pussy."
+    
+    static var datingIvanka = "If Ivanka weren't my daughter, perhaps I'd be dating her."
+    
+    static var droppingToKnees = "It must be a pretty picture, you (a female contestant on The Apprentice) dropping to your knees."
+    
+    static var muslimShutdown = "Donald J Trump is calling for a total and complete shutdown of Muslims entering the United States."
+    
+    static var meganKelly = "You could see there was blood coming out of her eyes, blood coming out of her... wherever."
+    
+    static var nastyWoman = "Such a nasty woman."
+    
+    static var fifthAve = "I could stand in the middle of 5th Avenue and shoot somebody and I wouldn't lose any voters."
+    
+    static var bombTheShit = "I would bomb the shit out of 'em."
+    
+    static var waterboarding = "I'd bring back waterboarding and I'd bring back a hell of a lot worse than waterboarding."
+    
+    static var handSize = "And he refered to my hands, if they're small, something else must be small. I gurantee there's no problem."
+    
+    static var obamaCofoundedIsis = "Isis is honoring President Obama. He is the founder of Isis. He's the founder of Isis. He's the founder! He founded Isis! And I would say the co-founder would be crooked Hillary Clinton."
+    
+    static var mccainNotAHero = "He's (John McCain) not a war hero. He's a war hero because he was captured. I like people who weren't captured."
+    
+    static var fuckHerSheWasMarried = "I did try and fuck her. She was married."
+    
+    static var secondAmendment = "Hillary wants to abolish, essentially abolish, the second ammendment...And if she gets to pick her judges, nothing you can do folks. Although the second ammendment people maybe there is. I don't know."
+    
+    static var takeTheirGuns = "I think that her bodyguards should drop all weapons. They should disarm. Right? Right? I think they should disarm immediately. What do you think? Yes? Take their guns away! She doesn't want guns....let's see what happens to her"
+    
+    static var buildAWall = "I will build a great, great wall on our southern border and I will have Mexico pay for that wall."
+    
+    static var noPuppet = "No puppet! No puppet! You're the puppet!"
+    
+    static var nucs = "Matthews: Will you tell the Middle East we're not using a nuclear weapon?\n Trump: I would never say that. I would never take any of my cards off the table.\n Mathews: How about Europe -- we won't use it in Europe?\n Trump: I'm not going to take it off the table."
+    
+    static var relationshipWithTheBlacks = "I have a great relationship with the blacks. I have always had a great relationship with the blacks."
+    
+    static var teenCreeping = "Before a show, I'll go backstage and everyone's getting dressed, and everything else, and you know, no men are anywhere, and I'm allowed to go in because I'm the owner of the pageant and therefore I'm inspecting it...You know, they're standing there with no clothes. And you see these incredible looking women, and so, I sort of get away with things like that."
+    
+    static var theyreRapists = "I did try and fuck her. She was married."
+    
+    static var carryOutInAStretcher = "I love the old days. You know what they used to do to guys like that when they were in a place like this? They'd be carried out on a stretcher, folks."
+    
+    static var obamaPositiveImactOnThugs = "Our great African-American President hasn't exactly had a positive impact on the thugs who are so happily and openly destroying Baltimore!"
+    
+    static var iWontLoseAPenny = "I'm not a schmuck. Even if the world is going to hell in a hand-basket, I won't lose a penny."
+    
+}
