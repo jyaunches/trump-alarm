acb9e16009ac98a3d0ab5f20751c997bef8a879e
Julie Yaunches <jmyaunch@gmail.com>
2016-11-03 08:37:36 -0400

    Adding photo taking to the quote screen.

--- trump-alarm/Controllers/QuotePlayViewController.swift
+++ trump-alarm/Controllers/QuotePlayViewController.swift
@@ -8,17 +8,23 @@
 
 import UIKit
 import SnapKit
+import AVFoundation
+import Photos
+import AssetsLibrary
 
-class QuotePlayViewController: UIViewController {
+class QuotePlayViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var trumpFaceImage: TrumpFace!
     var trumpQuote: TrumpQuote?
     var quotePlayer: QuotePlayer = QuotePlayer()
     var quoteLibrary = TrumpQuoteLibrary()
     var fixedWidth: CGFloat = 310
+    var photoManager = PhotoManager()
     
     @IBOutlet weak var trumpNameLabel: UILabel!
     @IBOutlet weak var quoteTextLabel: UITextView!
+    
+    @IBOutlet weak var iVotedButton: BorderedButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,11 +37,65 @@ class QuotePlayViewController: UIViewController {
             self.layoutQuoteLabel()
             self.playQuote()
         })
+
+        if TrumpAlarmUserDefaults.hasVoted {
+            iVotedButton.isHidden = true
+        }
     }
 
     override func viewWillDisappear(_ animated: Bool){
         super.viewWillDisappear(animated)
         quotePlayer.stop()
+    }
+
+    public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String: Any]) {
+        if let tempImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            photoManager.writeImage(image: tempImage)
+        }
+        NotificationManager.sharedInstance.cancelFutureNotififications()
+
+        TrumpAlarmUserDefaults.hasVoted = true
+        pushPostVoting()
+        picker.dismiss(animated: true)
+    }
+
+    func pushPostVoting() {
+        if let appDelegate = UIApplication.shared.delegate as? AppDelegate,
+        let postVC = appDelegate.storyboard.instantiateViewController(withIdentifier: "PostVotingController") as? PostVotingController {
+            self.navigationController?.pushViewController(postVC, animated: true)
+
+        }
+    }
+
+    func openCamera() {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.camera;
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+
+
+    @IBAction func votedClicked(_ sender: Any) {
+        if TrumpAlarmUserDefaults.hasVoted {
+            pushPostVoting()
+        } else {
+            let alertController = UIAlertController(title: "Did you really vote?", message: "Prove it! To silence this awful Trump noise, take a picture of yourself after voting.)", preferredStyle: UIAlertControllerStyle.alert)
+
+            let DestructiveAction = UIAlertAction(title: "Cancel", style: .cancel) {
+                (result: UIAlertAction) -> Void in
+                alertController.dismiss(animated: true, completion: nil)
+            }
+            let okAction = UIAlertAction(title: "I JUST VOTED", style: .default) {
+                (result: UIAlertAction) -> Void in
+                self.openCamera()
+            }
+            alertController.addAction(DestructiveAction)
+            alertController.addAction(okAction)
+            self.present(alertController, animated: true, completion: nil)
+        }
     }
     
     func layoutQuoteLabel() {
